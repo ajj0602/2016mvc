4b416445b1580d974a17e0fe24f964e43eefbdb6
HB6013\user <ajj0602@gmail.com>
2016-11-18 12:48:46 +0900

    maven적용 mvc2 스프링 따라하기 섻팅

---
+++ ex04/src/main/java/com/mvc2/controller/IndexImp.java
@@ -0,0 +1,18 @@
+package com.mvc2.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexImp implements InterController {
+
+	public String execute(HttpServletRequest req, HttpServletResponse res) {
+		
+		
+		
+		
+		return "main";
+	}
+
+	
+	
+}
---
+++ ex04/src/main/java/com/mvc2/core/DispatcherServlet.java
@@ -0,0 +1,54 @@
+package com.mvc2.core;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mvc2.controller.IndexImp;
+import com.mvc2.controller.InterController;
+import com.mvc2.controller.ListImp;
+
+
+public class DispatcherServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doDo(request, response);
+	}//doGet end
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doDo(request, response);
+	//전역으로 보낼게 있다면 여기서 셋어트리뷰트 해서 보내야할것.
+	}//doPost end
+	
+	protected void doDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getServletPath();
+		System.out.println(path);
+		
+		InterController ic = null;
+		
+		if(path.equals("/")){
+			ic=new IndexImp();
+
+		}else if(path.equals("/list.do")){
+			ic=new ListImp();
+		}
+		
+		
+		String url=ic.execute(request, response);
+		String prefix="/WEB-INF/page/";
+		String suffix=".jsp";
+		
+		request.getRequestDispatcher(prefix+url+suffix).forward(request, response);
+		
+		
+	}//doDo end
+
+}
+
+
+
